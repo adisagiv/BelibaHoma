1f9c614fe06bfe099979f317c407873e8fd3baa8
bareini <briany202@gmail.com>
2016-12-16 19:00:57 +0200

    Algorithm work + highchart

--- BelibaHoma.BLL/Interfaces/ITutorTraineeService.cs
+++ BelibaHoma.BLL/Interfaces/ITutorTraineeService.cs
@@ -54,6 +54,28 @@ namespace BelibaHoma.BLL.Interfaces
         /// <returns></returns>
         StatusModel Remove(int id);
 
+        /// <summary>
+        /// Get list of recommended TutorTrainee from db
+        /// </summary>
+        /// <param name="area"></param>
+        /// <returns></returns>
+        StatusModel<List<TutorTraineeModel>> GetRecommended(Area area);
+
+        /// <summary>
+        /// Run Auto Matching Algorithm
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        StatusModel RunAlgorithm(AlgorithmModel model);
+
+        /// <summary>
+        /// Add recomendation to DB
+        /// </summary>
+        /// <param name="tutor"></param>
+        /// <param name="trainee"></param>
+        /// <returns></returns>
+        StatusModel TutorTraineeAdd(TutorModel tutor, TraineeModel trainee);
+
     }
 
 }
