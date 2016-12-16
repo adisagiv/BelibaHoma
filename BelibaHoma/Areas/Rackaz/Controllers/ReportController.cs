ac359a691f858fe1d6593e521cfd749b13830227
adisagiv <adisagiv@gmail.com>
2016-12-16 19:02:47 +0200

    Added Report Controller

---
+++ BelibaHoma/Areas/Rackaz/Controllers/ReportController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BelibaHoma.Controllers;
+
+namespace BelibaHoma.Areas.Rackaz.Controllers
+{
+    public class ReportController : BaseController
+    {
+        [HttpPost]
+        public ActionResult HourStatistics()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AlertsStatistics()
+        {
+            return View();
+        }
+    }
+}
