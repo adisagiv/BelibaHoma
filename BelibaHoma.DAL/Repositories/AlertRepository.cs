9757ffda0f588e92103744138c8f7f0a4f9e9712
bareini <briany202@gmail.com>
2016-12-26 21:02:26 +0200

    Minor changes to home page + the very begining of Alert

---
+++ BelibaHoma.DAL/Repositories/AlertRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BelibaHoma.DAL.Interfaces;
+using Catel.Data.Repositories;
+
+namespace BelibaHoma.DAL.Repositories
+{
+    public class AlertRepository : EntityRepositoryBase<Alert, int>, IAlertRepository
+    {
+        public AlertRepository(BelibaHomaDBEntities context)
+            : base(context)
+        {
+
+        }
+    }
+}
